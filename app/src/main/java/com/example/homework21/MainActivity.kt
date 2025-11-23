0209bd897314482c41b24f51eb2314c7a72f2fce
MaksimMaksimchyk <maksim.maksimchyk@yandex.by>
2025-11-24 00:16:42 +0300

    Add Recycler

--- app/src/main/java/com/example/homework21/MainActivity.kt
+++ app/src/main/java/com/example/homework21/MainActivity.kt
@@ -1,20 +1,52 @@
 package com.example.homework21
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.homework21.MultiType.Author
+import com.example.homework21.MultiType.ButtonText
+import com.example.homework21.MultiType.ImageText
+import com.example.homework21.MultiType.MyMultiAdapter
+import com.example.homework21.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val listOfData = mutableListOf(Author(), ImageText(), ButtonText())
+        val myAdapter = MyMultiAdapter(listOfData)
+        binding.myRecycler.layoutManager = LinearLayoutManager(this)
+        binding.myRecycler.adapter = myAdapter
+
+        binding.buttonAdd.setOnClickListener {
+            when (Random.nextInt(0, 3)) {
+                0 -> listOfData.add(Author())
+                1 -> listOfData.add(ButtonText())
+                2 -> listOfData.add(ImageText())
+            }
+            Toast.makeText(this, "Added random card", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.buttonUpdate.setOnClickListener {
+            myAdapter.notifyDataSetChanged()
+        }
     }
+
+
 }
